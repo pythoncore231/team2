44908b86a3c6c9819ecaa00a4e25200515b13369
nataljchyk <nataliia.bas@gmail.com>
2017-03-09 22:35:46 +0200

    Initial commit HW_1

---
+++ nataliia.bas_HW_1.py
@@ -0,0 +1,7 @@
+first_number = 0
+second_number = 0
+first_number = int(raw_input("Please enter first_number: "))
+second_number = int(raw_input("Please enter second_number: "))
+Sum = first_number + second_number
+print("Sum of numbers = ")
+print(Sum)
