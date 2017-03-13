15b6a63a7c75f4afededa82c6176f7377d29ab99
vodachuk <vodachuk@gmail.com>
2017-03-13 11:37:49 +0200

    Added new branch and created new project
    A

---
+++ test3/src/test3.py
@@ -0,0 +1,15 @@
+'''
+Created on Mar 9, 2017
+
+@author: vodachuk
+'''
+
+print "hhhdh" + "jjdjdjjd"
+
+print "hhhdh" + "jjdjdjjd"
+print "hhhdh" + "jjdjdjjd"
+print "hhhdh" + "jjdjdjjd"
+print "hhhdh" + "jjdjdjjd"
+print "hhhdh" + "jjdjdjjd"
+print "hhhdh" + "jjdjdjjd"
+print "hhhdh" + "jjdjdjjd"
