70b2139ebc5306f1c09adcd4be8135579cd0026f
Horlatyi Ruslan <hrat1995@gmail.com>
2017-03-14 16:45:17 +0200

    HW

---
+++ HW_1/HW_02_01.py
@@ -0,0 +1,24 @@
+﻿my_file = open("simfli.txt", "r")
+my_string = my_file.read()
+
+print ("Было прочитано:")
+print (my_string.upper())
+print ("Кількість входжень better:")
+print (my_string.lower().count('better'))
+print ("Кількість входжень never:")
+print (my_string.lower().count('never'))
+print ("Кількість входжень is:")
+print (my_string.lower().count('is'))
+
+i=0
+m=""
+while i < len(my_string)-1:
+    x=my_string[i:i+1]
+    if x=="i":
+        z='&'
+    else:z=x
+    m=m+z
+    i=i+1
+
+print (m)
+my_file.close()
---
+++ HW_1/HW_02_02.py
@@ -0,0 +1,17 @@
+n = input('a = ')
+mult = 1
+summa = 0
+while n > 0:
+    if n % 10 != 0:
+        mult = mult * (n % 10)
+    summa = summa + n % 10
+    n = n // 10
+
+print ("Suma = {}".format(summa))
+print ("Dobutok = {}".format(mult))
+
+a1 = 14
+a2 = 52
+print ("{}, {}".format(a1,a2))
+a1,a2 = a2,a1
+print ("{}, {}".format(a1,a2))
