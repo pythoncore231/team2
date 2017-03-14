9c1420253434acad79ea1d2d906a807d3e8487ae
Ruslan Horlatyi <rhorltc@training.local>
2017-03-14 20:11:48 +0200

    14.031

--- HW_1/PW_03_01.py
+++ HW_1/PW_03_01.py
@@ -3,10 +3,12 @@ text = raw_input("Input text : ")
 finde = raw_input("Input find : ")
 
 i=0
+kount=0
 while i < len(text)-len(finde):
     x = text[i:i+len(finde)]
     if x == finde:
         print "Yes"
-        exit()
-    else: print "No"
+        kount = kount + 1
+#    else: print "No"
+    print "Count = {}".format(kount)
     i=i+1
