00826c77c94c3b9b137ebc75aa8632c31ee35dc3
Mrhrat <Hrat1995@gmail.com>
2017-03-22 09:22:07 +0200

    HW_02_02

--- HW_1/HW_02_02.py
+++ HW_1/HW_02_02.py
@@ -1,3 +1,6 @@
+# -*- coding: utf-8 -*-
+import math
+
 n = input('a = ')
 mult = 1
 summa = 0
@@ -15,3 +18,35 @@ a2 = 52
 print ("{}, {}".format(a1,a2))
 a1,a2 = a2,a1
 print ("{}, {}".format(a1,a2))
+
+x = float(input('x = '))
+y = float(input('y = '))
+z = float(input('z = '))
+
+a = (math.fabs(x-1.)**(1./2)-math.fabs(y)**(1./3))/(1.+x**2/2+y**2/4)
+b = x*(math.atan(z)) + math.exp(-(x+3.))
+print "A) a){:11.5f}\tb){:11.5f}".format(a,b)
+
+a = (3 + math.exp(y-1.))/(1+x**2*math.fabs(y - math.tan(z)))
+b = 1 + math.fabs(y - x) + (y - x)**2/2 + math.fabs(y - x)**3/3
+print "Б) a){:11.5f}\tb){:11.5f}".format(a,b)
+
+a = (1 + y)*(x + y/(x**2 + 4))/(math.exp(-x-2.)+ 1./(x**2+4))
+b = (1 + math.cos(y - 2.))/(x**4/2. + math.sin(z)**2)
+print "В) a){:11.5f}\tb){:11.5f}".format(a,b)
+
+a = y + x/(y**2 + math.fabs(x**2/(y+x**3/3.)))
+b = 1 + math.tan(z/2)**2
+print "Г) a){:11.5f}\tb){:11.5f}".format(a,b)
+
+a = 2.*math.cos(x - math.pi/6)/(1./2 + math.sin(y)**2)
+b = 1 + z**2/(3 + z**2./5)
+print "Д) a){:11.5f}\tb){:11.5f}".format(a,b)
+
+a = (1+math.sin(x+y)**2)/(2+math.fabs(x-2*x/(1+x**2*y**2)))+x
+b = math.cos(math.atan(1./z))**2
+print "Е) a){:11.5f}\tb){:11.5f}".format(a,b)
+
+a = math.log10((y - math.fabs(x)**(1./2))*(x - y/(z + x**2/4)))
+b = x - x**2./math.factorial(3) + x**5/math.factorial(5)
+print "Ж) a){:11.5f}\tb){:11.5f}".format(a,b)
