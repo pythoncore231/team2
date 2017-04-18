873f6b9a1eec4a8a0f54701002904f120e053548
Lubomyr Halamaga <lhalamtc@training.local>
2017-04-18 20:13:02 +0300

    add user entity

--- flask_app/app.py
+++ flask_app/app.py
@@ -3,12 +3,12 @@ import os.path
 from flask import Flask, request, redirect,  render_template
 from flask_sqlalchemy import SQLAlchemy
 
-from form.room import RoomForm
+from form.room import RoomForm, UserForm
 
 app = Flask(__name__)
 
 basedir = os.path.abspath(os.path.dirname(__file__))
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'app.db')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'app.sqlite')
 
 db = SQLAlchemy(app)
 
@@ -21,10 +21,25 @@ class Room(db.Model):
     def __repr__(self):
         return '<Room {} {}>'.format(self.name, self.capacity)
 
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    firstname = db.Column(db.String(20), nullable=False)
+    lastname = db.Column(db.String(20), nullable=False)
+    age = db.Column(db.Integer, nullable=True)
+    
+        
+
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    return """
+            <a href='http://localhost:5000/user'>user</a><br>
+            <a href='http://localhost:5000/user/add'>add user</a><br>
+            <a href='http://localhost:5000/room'>room</a>"""
+
+@app.route('/test')
+def hello_world_test():
+    return 'test Hello, World!'
 
 @app.route('/room', methods=['GET', 'POST'])
 def room():
@@ -37,6 +52,27 @@ def room():
 
     rooms = Room.query.all()
     return render_template('room.html', form=form, rooms=rooms)
+
+
+
+@app.route('/user', methods=['GET'])
+def user_get():
+    users = User.query.all()
+    return render_template('user.html', us=users)
+
+@app.route('/user/add', methods=['GET','POST'])
+def user_add():
+    form = UserForm(request.form)
+    if request.method == 'POST' and form.validate():
+        user = User(firstname = form.firstname.data,
+                    lastname = form.lastname.data,
+                    age = form.age.data)
+        db.session.add(user)
+        db.session.commit()
+        return redirect('/user')
+    return render_template('user_add.html', form=form)
+
+
 
 
 
--- flask_app/form/room.py
+++ flask_app/form/room.py
@@ -3,3 +3,9 @@ from wtforms import Form, StringField, IntegerField, validators
 class RoomForm(Form):
     name = StringField('Name', [validators.Length(min=4, max=10)])
     capacity = IntegerField('Capacity')
+
+
+class UserForm(Form):
+    firstname= StringField('First name', [validators.Length(min=1, max=5)])
+    lastname = StringField('Last name', [validators.Length(min=10, max=20)])
+    age = IntegerField('Age')
